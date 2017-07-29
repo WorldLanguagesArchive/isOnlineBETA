76ee1bf7ddeb2d46996998fb8f46097437239b81
World_Languages <mundofinky@gmail.com>
2017-07-29 01:17:11 -0300

    Add files via upload

--- background.js
+++ background.js
@@ -3,35 +3,34 @@ chrome.runtime.onInstalled.addListener(function(details){
         chrome.tabs.create({url:"https://scratchtools.tk/isonline/register/"});}
     if(details.reason == "update"){
         chrome.tabs.create({url: "https://scratch.mit.edu/isonline-extension/update"});
-		}
+    }
 });
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-		console.log(request);
+        console.log(request);
         if (request.action == "reload") {
             location.reload();}
-		if (request.setuninstallurl != null) {
-		chrome.runtime.setUninstallURL("https://scratchtools.tk/isonline/uninstall/?user="+request.setuninstallurl.name+"&key="+request.setuninstallurl.key);}
-		
-		if(request.color === ""){
-		localStorage.setItem("iOstatus","online");
-		chrome.browserAction.getBadgeText({}, function(result) {
-		if(result===" "){chrome.browserAction.setBadgeText({text: ""});}
-		chrome.browserAction.setBadgeBackgroundColor({color: "#0000FF"});
-		});
-		}
+        if (request.setuninstallurl) {
+            chrome.runtime.setUninstallURL("https://scratchtools.tk/isonline/uninstall/?user="+request.setuninstallurl.name+"&key="+request.setuninstallurl.key);}
+        if(request.color === ""){
+            localStorage.setItem("iOstatus","online");
+            chrome.browserAction.getBadgeText({}, function(result) {
+                if(result===" "){chrome.browserAction.setBadgeText({text: ""});}
+                chrome.browserAction.setBadgeBackgroundColor({color: "#0000FF"});
+            });
+        }
         if (request.color == "orange") {
-		localStorage.setItem("iOstatus","absent");
+            localStorage.setItem("iOstatus","absent");
             badge("#FFA500");}
         if (request.color == "gray") {
-		localStorage.setItem("iOstatus","dnd");
+            localStorage.setItem("iOstatus","dnd");
             badge("#808080");}
-		if (request.color == "red") {
-		localStorage.setItem("iOstatus","offline");
+        if (request.color == "red") {
+            localStorage.setItem("iOstatus","offline");
             badge("#FF0000");}
     });
-	
+
 setInterval(function(){
     chrome.tabs.query({url:"https://scratch.mit.edu/*"}, function(tabs) {
         if (tabs.length===0){chrome.browserAction.setBadgeText({text: ""});}
@@ -40,9 +39,9 @@ setInterval(function(){
 
 
 function badge(thecolor) {
-	chrome.browserAction.getBadgeText({}, function(result) {
-	if(result===""){
-    chrome.browserAction.setBadgeText({text: " "});}
-	chrome.browserAction.setBadgeBackgroundColor({color: thecolor});
-});
+    chrome.browserAction.getBadgeText({}, function(result) {
+        if(result===""){
+            chrome.browserAction.setBadgeText({text: " "});}
+        chrome.browserAction.setBadgeBackgroundColor({color: thecolor});
+    });
 }
